6356ab954c97fa3054108dc0a913f7a965649f37
Bryan Berry <bryan.berry@gmail.com>
2013-03-27 16:39:14 +0100

    fix for #38

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,5 @@
+recipes = []
+
 # Hack to install Gem immediately pre Chef 0.10.10 (CHEF-2879)
 chef_gem "minitest" do
   version node['minitest']['gem_version']
@@ -12,16 +14,20 @@ Gem.clear_paths
 # Ensure minitest gem is utilized
 require "minitest-chef-handler"
 
-if Chef::VERSION < "11.0"
-  seen_recipes = node.run_state[:seen_recipes]
-  recipes = seen_recipes.keys.each { |i| i }
-else
-  recipes = run_context.loaded_recipes
-end
 
-if recipes.empty? and Chef::Config[:solo]
-  #If you have roles listed in your run list they are NOT expanded
-  recipes = node.run_list.map {|item| item.name if item.type == :recipe }
+ruby_block "load list of recipes" do
+  block do
+    if Chef::VERSION < "11.0"
+      seen_recipes = node.run_state[:seen_recipes]
+      recipes = seen_recipes.keys.each { |i| i }
+    else
+      recipes = run_context.loaded_recipes
+    end
+    if recipes.empty? and Chef::Config[:solo]
+      #If you have roles listed in your run list they are NOT expanded
+      recipes = node.run_list.map {|item| item.name if item.type == :recipe }
+    end
+  end
 end
 
 # Directory to store cookbook tests
@@ -45,61 +51,64 @@ ruby_block "delete tests from old cookbooks" do
 end
 
 # Search through all cookbooks in the run list for tests
-recipes.each do |recipe|
-  # recipes is actually a list of cookbooks and recipes with :: as a
-  # delimiter
-  cookbook_name,recipe_name = recipe.split('::')
-  recipe_name ||= "default"
+ruby_block "load tests" do
+  block do
+    recipes.each do |recipe|
+      # recipes is actually a list of cookbooks and recipes with :: as a
+      # delimiter
+      cookbook_name,recipe_name = recipe.split('::')
+      recipe_name ||= "default"
+      
+      if recipes.any? { |recipe| recipe.split('::').first == cookbook_name }
+        # create the parent directory
+        dir = Chef::Resource::Directory.new("#{node['minitest']['path']}/#{cookbook_name}", run_context)
+        dir.recursive(true)
+        dir.run_action :create
 
-  if node['recipes'].any? { |recipe| recipe.split('::').first == cookbook_name }
-    # create the parent directory
-    directory "#{node['minitest']['path']}/#{cookbook_name}" do
-      recursive true
-    end
+        ckbk = run_context.cookbook_collection[cookbook_name]
+        begin
+          # This will raise at compile-time if we can't find the cookbook_file
+          ckbk.preferred_manifest_record(node, 'files', "tests/minitest/#{recipe_name}_test.rb")
+          ckbk_f = Chef::Resource::CookbookFile.new("tests-#{cookbook_name}-#{recipe_name}", run_context)
+          ckbk_f.source "tests/minitest/#{recipe_name}_test.rb"
+          ckbk_f.cookbook cookbook_name
+          ckbk_f.path "#{node['minitest']['path']}/#{cookbook_name}/#{recipe_name}_test.rb"
+          ckbk_f.ignore_failure true
+          ckbk_f.run_action :create
+        rescue Chef::Exceptions::FileNotFound
+          Chef::Log.warn "No tests found for #{recipe}!"
+        end
 
-    ckbk = run_context.cookbook_collection[cookbook_name]
-    begin
-      # This will raise at compile-time if we can't find the cookbook_file
-      ckbk.preferred_manifest_record(node, 'files', "tests/minitest/#{recipe_name}_test.rb")
+        begin
+          # This will raise at compile-time if we can't find the directory
+          ckbk.preferred_manifest_records_for_directory(node, 'files', 'tests/minitest/support')
 
-      cookbook_file "tests-#{cookbook_name}-#{recipe_name}" do
-        source "tests/minitest/#{recipe_name}_test.rb"
-        cookbook cookbook_name
-        path "#{node['minitest']['path']}/#{cookbook_name}/#{recipe_name}_test.rb"
-        ignore_failure true
+          # copy any helper files from the support directory
+          ckbk_d = Chef::Resource::RemoteDirectory.new("tests-support-#{cookbook_name}-#{recipe_name}", run_context)
+          ckbk_d.source "tests/minitest/support"
+          ckbk_d.cookbook cookbook_name
+          ckbk_d.path "#{node['minitest']['path']}/#{cookbook_name}/support"
+          ckbk_d.recursive true
+          ckbk_d.ignore_failure true
+          ckbk_d.run_action :create
+        rescue Chef::Exceptions::FileNotFound
+          Chef::Log.debug "No support files found for #{recipe}."
+        end
       end
-    rescue Chef::Exceptions::FileNotFound
-      Chef::Log.warn "No tests found for #{recipe}!"
     end
 
-    begin
-      # This will raise at compile-time if we can't find the directory
-      ckbk.preferred_manifest_records_for_directory(node, 'files', 'tests/minitest/support')
+    options = {
+      :path    => "#{node['minitest']['path']}/#{node['minitest']['tests']}",
+      :verbose => node['minitest']['verbose']}
+    # The following options can be omited
+    options[:filter]     = node['minitest']['filter'] if node['minitest'].include? 'filter'
+    options[:seed]       = node['minitest']['seed'] if node['minitest'].include? 'seed'
+    options[:ci_reports] = node['minitest']['ci_reports'] if node['minitest'].include? 'ci_reports'
 
-      # copy any helper files from the support directory
-      remote_directory "tests-support-#{cookbook_name}-#{recipe_name}" do
-        source "tests/minitest/support"
-        cookbook cookbook_name
-        path "#{node['minitest']['path']}/#{cookbook_name}/support"
-        recursive true
-        ignore_failure true
-      end
-    rescue Chef::Exceptions::FileNotFound
-      Chef::Log.debug "No support files found for #{recipe}."
-    end
+    handler = MiniTest::Chef::Handler.new(options)
+
+    Chef::Log.info("Enabling minitest-chef-handler as a report handler")
+    Chef::Config.send("report_handlers").delete_if {|v| v.class.to_s.include? MiniTest::Chef::Handler.to_s}
+    Chef::Config.send("report_handlers") << handler
   end
 end
-
-options = {
-  :path    => "#{node['minitest']['path']}/#{node['minitest']['tests']}",
-  :verbose => node['minitest']['verbose']}
-# The following options can be omited
-options[:filter]     = node['minitest']['filter'] if node['minitest'].include? 'filter'
-options[:seed]       = node['minitest']['seed'] if node['minitest'].include? 'seed'
-options[:ci_reports] = node['minitest']['ci_reports'] if node['minitest'].include? 'ci_reports'
-
-handler = MiniTest::Chef::Handler.new(options)
-
-Chef::Log.info("Enabling minitest-chef-handler as a report handler")
-Chef::Config.send("report_handlers").delete_if {|v| v.class.to_s.include? MiniTest::Chef::Handler.to_s}
-Chef::Config.send("report_handlers") << handler
