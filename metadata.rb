d64f5086008bba221723663ca05ff9a2ca3b6655
Bryan Berry <bryan.berry@gmail.com>
2013-05-07 17:26:16 +0200

    bump version for new release

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "bryan.berry@gmail.com"
 license          "Apache 2.0"
 description      "Installs and configures minitest-chef-handler"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 
 depends "chef_handler"
