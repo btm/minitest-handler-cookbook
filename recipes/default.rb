bb3043bae83309192cf2fe8ec091f441364a02f5
dpetzel <davidpetzel@gmail.com>
2013-06-13 23:10:54 -0400

    force a download of testing files from chef server

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,9 +14,20 @@ Gem.clear_paths
 # Ensure minitest gem is utilized
 require "minitest-chef-handler"
 
+scratch_dir = ::File.join(Chef::Config[:file_cache_path], "minitest_scratch")
+
 [:delete, :create].each do |action|
   directory "minitest test location" do
     path node[:minitest][:path]
+    owner node[:minitest][:owner]
+    group node[:minitest][:group]
+    mode node[:minitest][:mode]
+    recursive true
+    action action
+  end
+  
+  directory scratch_dir do
+    path scratch_dir
     owner node[:minitest][:owner]
     group node[:minitest][:group]
     mode node[:minitest][:mode]
@@ -59,17 +70,52 @@ ruby_block "load tests" do
       dir = Chef::Resource::Directory.new("#{node[:minitest][:path]}/#{cookbook_name}", run_context)
       dir.recursive(true)
       dir.run_action :create
+      
+      # https://github.com/btm/minitest-handler-cookbook/issues/42
+      # Ensure we download cookbooks from Chef Server so the rest
+      # of this code works. It would appear that if the file is not
+      # in the resource list, its never downloaded, so we have to
+      # 'force' a download. **NOTE** We force the download to a
+      # directory we don't use. This looks odd, but actually has a purpose
+      # We don't want to download them directly into the minitest dir
+      # as that results in tests running for recipes not in the runlist
+      # and the provider is going to copy them into the cookbook cache
+      # automatically, just essentially set the path to a bogus location
+     
+      ckbk = run_context.cookbook_collection[cookbook_name]
+      # Support both old and new locations
+      old_path = "tests/minitest"
+      new_path = "test"
+      
+      [old_path, new_path].each do |test_path|
+        begin
+          # This will raise at compile-time if we can't find the directory
+          ckbk.preferred_manifest_records_for_directory(node, 'files', old_path)
 
+          # copy the test files
+          ckbk_d = Chef::Resource::RemoteDirectory.new("tests-support-#{cookbook_name}-#{recipe_name}", run_context)
+          ckbk_d.source test_path
+          ckbk_d.cookbook cookbook_name
+          ckbk_d.path ::File.join(scratch_dir, cookbook_name)
+          ckbk_d.recursive true
+          ckbk_d.ignore_failure true
+          ckbk_d.run_action :create
+        rescue Chef::Exceptions::FileNotFound
+          Chef::Log.debug "No test file directory found at #{test_path}."
+        end
+      end
+      
       tests = Array(Chef::Config[:cookbook_path]).map do |cookbook_path|
         ::Dir["#{cookbook_path}/#{cookbook_name}/files/default/**/#{recipe_name}_test.rb"]
       end.flatten(1)
       unless tests.empty?
         FileUtils.cp tests, "#{node[:minitest][:path]}/#{cookbook_name}/"
       end
-      
+
       support_files = Array(Chef::Config[:cookbook_path]).map do |cookbook_path|
         ::Dir["#{cookbook_path}/#{cookbook_name}/files/default/**/*helper*.rb"]
       end.flatten(1)
+      
       unless support_files.empty?
         # do this in a loop to preserve directory structure
         # for backwards compatibility for dumb idea of putting support files in support/
