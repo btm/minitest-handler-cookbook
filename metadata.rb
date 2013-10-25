5bc1df660589d8535576a399780b3adb63503a10
dpetzel <dave@dpetzel.info>
2013-10-25 19:16:45 -0400

    fix version number so the release gets released properly

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "davidpetzel@gmail.com"
 license          "Apache 2.0"
 description      "Installs and configures minitest-chef-handler"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.1"
+version          "1.1.0"
 
 depends "chef_handler"
