36f701f04f7022d87c683ae4095baf1193e465ec
dpetzel <davidpetzel@gmail.com>
2014-07-25 22:54:31 -0400

    address deprecation warnings after upgrading chefspec/rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,10 @@ require 'chefspec/berkshelf'
 # Require all our libraries
 Dir['libraries/*.rb'].each { |f| require File.expand_path(f) }
 
+RSpec.configure do |config|
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
+
 at_exit { ChefSpec::Coverage.report! }
