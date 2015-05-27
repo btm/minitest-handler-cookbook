48e0d2a4900dcb794259181178d68f453b5c2f27
heathsnow <heath.snow@changepoint.com>
2015-05-27 13:54:34 -0700

    use artifactory and updated minitest-chef-handler gem with chef lockdown to v11

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,18 +12,36 @@ chef_gem 'minitest' do
   only_if { Chef::VERSION.to_f < 10.10 }
 end.run_action(:install)
 
-chef_gem 'minitest-chef-handler' do
-  version node[:minitest][:chef_handler_gem_version]
-  action :nothing
-  # I won't pretend I understand WHY this works, but since the release of
-  # Chef 11.8, this was causing errors related to the PUMA Gem
-  # http://lists.opscode.com/sympa/arc/chef/2013-10/msg00592.html
-  # I tried using the conservative flag, as well as a few other hacks
-  # but for whatever reason, simply retrying once works. The initial
-  # attempt still fails with the error in that thread, however
-  # the retry succeeds...
-  retries 1
-end.run_action(:install)
+if node[:minitest][:chef_handler_gem_source]
+  remote_str = "#{Chef::Config[:file_cache_path]}/minitest-chef-handler-"
+  remote_str << "#{node[:minitest][:chef_handler_gem_version]}.gem"
+
+  remote_src = "#{node[:minitest][:chef_handler_gem_source]}/"
+  remote_src << "minitest-chef-handler-#{node[:minitest][:chef_handler_gem_version]}.gem"
+
+  remote_file remote_str do
+    source remote_src
+  end
+  chef_gem 'minitest-chef-handler' do
+    version node[:minitest][:chef_handler_gem_version]
+    action :nothing
+    source remote_str
+    retries 1
+  end.run_action(:install)
+else
+  chef_gem 'minitest-chef-handler' do
+    version node[:minitest][:chef_handler_gem_version]
+    action :nothing
+    # I won't pretend I understand WHY this works, but since the release of
+    # Chef 11.8, this was causing errors related to the PUMA Gem
+    # http://lists.opscode.com/sympa/arc/chef/2013-10/msg00592.html
+    # I tried using the conservative flag, as well as a few other hacks
+    # but for whatever reason, simply retrying once works. The initial
+    # attempt still fails with the error in that thread, however
+    # the retry succeeds...
+    retries 1
+  end.run_action(:install)
+end
 
 Gem.clear_paths
 # Ensure minitest gem is utilized
