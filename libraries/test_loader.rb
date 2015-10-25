6019e245add7431537af9072dcb511f921f140c9
dpetzel <davidpetzel@gmail.com>
2015-10-25 19:47:04 -0400

    mend

--- libraries/test_loader.rb
+++ libraries/test_loader.rb
@@ -256,11 +256,11 @@ module MinitestHandler
         relative_file_name = cb_file.dup
         Array(Chef::Config[:cookbook_path]).each do |cb_path|
           this_cb_path = ::File.absolute_path(
-          # Ensure we have the trailing slash
+            # Ensure we have the trailing slash
             ::File.join(cb_path, cookbook_name, '/')
           )
           # Ensure the full cookbook path is not part of relative name
-          dir_prefix = %r{[A-Za-z]?:?#{this_cb_path}}
+          dir_prefix = /[A-Za-z]?:?#{this_cb_path}/
           relative_file_name.gsub!(dir_prefix, '')
 
           files_prefix = ::File.join('files', 'default', '/')
